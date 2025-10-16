777156dd33c27080f79af0ac4a983786318f8187
RefinedWiki Build Agent <bamboo@refinedwiki.com>
2025-10-16 22:47:37 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Emil test render deploy</h1>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
+
+sty
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
